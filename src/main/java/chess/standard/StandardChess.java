e2f33118d7e60a75f13bcbe255ebd5999e1c79e3
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2021-01-04 20:48:31 +0100

    I added some folders and created a Chess interface. Then I changed the name of the class Chess to StandardChess and made it implement the interface

---
+++ src/main/java/chess/standard/StandardChess.java
@@ -0,0 +1,17 @@
+package main.java.chess.standard;
+
+import main.java.chess.framework.Chess;
+
+/*
+Iteration 1
+A chess board with 16 white pawns and 16 black pawns, all being able to move forward 1 step at the time,
+until they meet an opponent pawn.
+White wins after 10 turns.
+
+ */
+public class StandardChess implements Chess {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
